551dfbf9804c777ec6b7e92dd6ff32f42e93d5be
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-11-18 11:21:29 +0100

    recipe-item.html completed

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientModel } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: IngredientModel[] = [
+    new IngredientModel('Pasta', 500),
+    new IngredientModel('Pomodoro', 2),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
