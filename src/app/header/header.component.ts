f261eb35574822ffd7805ba9c6214335b3403078
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-11-18 11:34:04 +0100

    Show/Hide recipes or shopping list using an event

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
   collapsed: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(feature: string) {
+    
+    this.featureSelected.emit(feature);
+  }
 }
