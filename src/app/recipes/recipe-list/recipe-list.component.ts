c71c116559c0002c3b0eefafbb275614b326b820
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-12-16 10:11:16 +0100

    Starting the RecipesService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { RecipeModel } from 'src/app/models/recipe.model';
+import { RecipesService } from 'src/app/recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,35 +9,16 @@ import { RecipeModel } from 'src/app/models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeSelectedInList = new EventEmitter<RecipeModel>();
-  recipes: RecipeModel[] = [
-    new RecipeModel(
-      'Spaghetti alla chitarra',
-      'Un particolare tipo di pasta che...',
-      'https://images.lacucinaitaliana.it/wp-content/uploads/2020/06/03204258/Pasta-alla-chitarra-con-ragu-di-polo.jpg'
-    ),
-    new RecipeModel(
-      'Lasagne alla bolognese',
-      'Pasta emiliano molto calorica sempre presente nei menù',
-      'https://images.lacucinaitaliana.it/gallery/119939/Big/be774eb0-09eb-4e1e-940d-72e38cea3ac3.jpg'
-    ),
-    new RecipeModel(
-      'Gnocchi ai formaggi',
-      'Gnocchi al formaggio',
-      'https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2017/05/1495792337_13c855513b63705bb079c377ed01563d4eb12ccc-223759579.jpg?w=580'
-    ),
-    new RecipeModel(
-      'Tiramisù',
-      'Dolce bono',
-      'https://ricetta.it/Uploads/Imgs/tiramisu-classico.jpg'
-    ),
-  ];
 
-  selectedRecipe:RecipeModel=this.recipes[0];
-  constructor() {}
+  recipes: RecipeModel[] = [];
+  selectedRecipe: RecipeModel = this.recipes[0];
+  constructor(private recipesService: RecipesService) {
+    this.recipes=recipesService.recipes;
+  }
 
   ngOnInit(): void {}
 
-  onRecipeSelected(recipe:RecipeModel){
+  onRecipeSelected(recipe: RecipeModel) {
     this.recipeSelectedInList.emit(recipe);
   }
 }
